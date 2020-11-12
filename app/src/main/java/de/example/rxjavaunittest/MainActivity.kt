1e72b5bc184c58a1cc1e515f434ad77cf55b1c1f
Fahri MacBook <fahri-can@web.de>
2020-11-12 15:18:12 +0100

    Observe LiveData in Activity.

--- app/src/main/java/de/example/rxjavaunittest/MainActivity.kt
+++ app/src/main/java/de/example/rxjavaunittest/MainActivity.kt
@@ -2,23 +2,40 @@ package de.example.rxjavaunittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.example.rxjavaunittest.databinding.ActivityMainBinding
 import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val service: JsonPlaceholderApi = JsonPlaceholderApiService.getClient()
     private val repository = Repository(service)
-    private val viewModel =  MyViewModel(repository, Schedulers.io())
+    private val viewModel = MyViewModel(repository, Schedulers.io())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        observeLiveData()
     }
 
-    override fun onResume() {
-        super.onResume()
-        //viewModel.getAllPosts()
-        viewModel.getPost()
+    private fun observeLiveData() {
+        observePost()
+        observePosts()
+    }
+
+    private fun observePost() {
+        viewModel.singlePost.observe(this, { post ->
+            binding.postTitle.text = "Title: ${post.title}"
+        })
+    }
+
+    private fun observePosts() {
+        viewModel.posts.observe(this, { posts ->
+            binding.numberOfPosts.text = "Number of posts: ${posts.size}"
+        })
     }
 
 }
